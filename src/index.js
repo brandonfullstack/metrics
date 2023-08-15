2c926e0d9674e83baeb6ca0368bf8498b6d84074
Sean Norwood <norwood.sean@gmail.com>
2023-08-14 22:25:43 -0500

    Replace bootstrap css with tailwindcss
    Co-authored-by: brian yoder <brnydr@users.noreply.github.com>
    Co-authored-by: Julien <julienfullstack@users.noreply.github.com>
    Co-authored-by: brandonfullstack <brandonfullstack@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
+import "./css/styles.css";
+import "./css/global.css";
 
 const applications = [
   "CAREWare",
