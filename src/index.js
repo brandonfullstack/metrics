7fae27d1ac4df54d8849533beed99442e3507869
brnydr <brianwilliamyoder@gmail.com>
2023-08-20 10:09:16 -0700

    name functions, add eventlisteners

--- src/index.js
+++ src/index.js
@@ -27,22 +27,24 @@ const metrics = {
   voicemail: "Voicemail",
 };
 
-const formEl = document.getElementById("theForm");
+// const formEl = document.getElementById("theForm");
 
-formEl.addEventListener("submit", (event) => {
-  event.preventDefault();
-  console.log(event.target.elements[0].value);
-});
+// formEl.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   console.log(event.target.elements[0].value);
+// });
+
 const tableEl = document.getElementById("apps");
 
-const headerRow = document.createElement("tr");
-for (let j = 0; j < applications.length; j++) {
-  const th = document.createElement("th");
-  th.textContent = applications[j];
-  headerRow.appendChild(th);
+function createTableHeaderRow() {
+  const headerRow = document.createElement("tr");
+  for (const app of applications) {
+    const th = document.createElement("th");
+    th.textContent = app;
+    headerRow.appendChild(th);
+  }
+  tableEl.appendChild(headerRow);
 }
-
-tableEl.appendChild(headerRow);
 
 function renderTableColumn(text, id) {
   const tableRow = document.createElement("tr");
@@ -53,7 +55,6 @@ function renderTableColumn(text, id) {
   tableHeader.textContent = text;
 
   tableRow.appendChild(tableHeader);
-
 
   applications.slice(1).forEach((app) => {
     const tableData = document.createElement("td");
@@ -73,12 +74,17 @@ function renderTableColumn(text, id) {
   tableEl.appendChild(tableRow);
 }
 
-for (const key in metrics) {
-  const element = metrics[key];
-  renderTableColumn(element, key);
+function initializeTable() {
+  createTableHeaderRow();
+
+  for (const key in metrics) {
+    const element = metrics[key];
+    renderTableColumn(element, key);
+  }
 }
 
-// UI Logic
+
+window.addEventListener("load", initializeTable);
 
 document.querySelector("form").addEventListener("click", function() {
 
@@ -90,78 +96,3 @@ document.querySelector("form").addEventListener("click", function() {
     firstButton.click()
   }
 });
-
-
-
-
-// metrics.forEach((metric) => {
-//   renderTableColumn(metric);
-// });
-
-
-
-  
-  // tableEl.appendChild(tableRow);
-
-
-// function addTableHeaders(headers) {
-//   headers.forEach((header) => {
-//     header = 
-//     const th = document.createElement('th');
-//     th.classList.add('py-2', 'px-4', 'font-bold', 'bg-gray-200');
-//     th.textContent = header;
-//     tr.appendChild(th);
-//   });
-
-//   thead.appendChild(tr);
-//   table.appendChild(thead);
-// }
-
-// for (const key in metrics) {
-//   const elementHeader = applications[key];
-//   addTableHeaders(elementHeader, key);
-// }
-
-
-// const table = document.createElement("table");
-// table.classList.add("table");
-
-// for (let i = 0; i < metrics.length; i++) {
-//   const row = document.createElement("tr");
-
-
-
-//     // Create header cells for applications
-    // for (let j = 0; j < applications.length; j++) {
-    //   const th = document.createElement("th");
-    //   th.textContent = applications[j];
-    //   row.appendChild(th);
-    // }
-//   } else {
-//     // Create metric label cell
-//     const th = document.createElement("th");
-
-//     th.classList.add("text-nowrap", "text-end", "p-3");
-//     th.textContent = metrics[i];
-//     row.appendChild(th);
-
-//     // Create input cells for metrics
-//     for (let j = 0; j < applications.length; j++) {
-//       const td = document.createElement("td");
-//       const input = document.createElement("input");
-
-//       input.type = "number";
-//       input.min = 0;
-//       input.classList.add("form-control");
-//       input.id = `${applications[j]}-${metrics[i]}`;
-//       input.placeholder = "";
-//       input.tabIndex = i + metrics.length * j;
-//       td.appendChild(input);
-//       row.appendChild(td);
-//     }
-//   }
-
-//   table.appendChild(row);
-// }
-
-// document.body.appendChild(table);
