5fd4adf2c23e4b42a5f6bb35bb633006748eb1de
Brandon Wright <brandonfullstack@gmail.com>
2023-08-20 20:01:44 -0400

    WIP: removed accordian added puppy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./css/styles.css";
-import Chart from 'chart.js/auto';
+// import Chart from 'chart.js/auto';
 
 const applications = [
   "",
